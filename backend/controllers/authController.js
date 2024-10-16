aa1a73572a9ba32b287c757440af59248558423a
ayushdixit210801 <ayushdixit210801@gmail.com>
2024-10-16 21:00:15 +0000

    added authentication routes

---
+++ backend/controllers/authController.js
@@ -0,0 +1,86 @@
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
+	try {
+		const { fullName, username, password, confirmPassword, gender } = req.body;
+
+		if (password !== confirmPassword) {
+			return res.status(400).json({ message: "Passwords do not match" });
+		}
+
+		const user = await User.findOne({ username });
+
+		if (user) {
+			return res.status(400).json({ message: "User already exists" });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+		const newUser = new User({
+			fullName,
+			username,
+			password: hashedPassword,
+			gender,
+			profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+		});
+
+		if (newUser) {
+			generateTokenAndSetCookie(newUser._id, res);
+			await newUser.save();
+
+			res.status(201).json({
+				_id: newUser._id,
+				fullName: newUser.fullName,
+				username: newUser.username,
+				gender: newUser.gender,
+				profilePic: newUser.profilePic,
+			});
+		} else {
+			res.status(400).json({ message: "Invalid user data" });
+		}
+	} catch (error) {
+		console.error("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export const login = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = user && (await bcrypt.compare(password, user.password));
+
+		if (!isPasswordCorrect) {
+			return res.status(400).json({ message: "Invalid username or password" });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			gender: user.gender,
+			profilePic: user.profilePic,
+		});
+	} catch (error) {
+		console.error("Error in login controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export const logout = async (req, res) => {
+	try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
+		console.error("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
 
-dotenv.config();
+import authRoutes from "./routes/authRoutes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-	//root route http://localhost:5000/
-	res.send("Hello World");
-});
+dotenv.config();
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server running on port ${PORT}`);
+});
